78f5670aa6714bdc42fd601dce155e6234818105
azureblue <kk0xf0@gmail.com>
2017-04-03 21:02:23 +0200

    supporting non square buffer, adding rooms

--- ca.js
+++ ca.js
@@ -1,6 +1,4 @@
-function CA(canvas, board) {
-    const size = board.getWidth();
-    const bufferSize = size + 2;
+function CA(canvas, bufferWidth, bufferHeight, roomSize) {
     const minZoom = 1 / (1 << 4);
     var zoom = 2;
     var srcTex, destTex;
@@ -36,11 +34,13 @@ function CA(canvas, board) {
     this.iterateAndRender = function() {
         currentProgram = caProgram;
         currentProgram.use();
-        caProgram.setCASize(bufferSize);
+        caProgram.setCASize(bufferWidth, bufferHeight);
+        caProgram.setCARoomSize(roomSize, roomSize);
         gl.bindFramebuffer(gl.FRAMEBUFFER, frameBufferDest);
-        gl.viewport(0, 0, bufferSize, bufferSize);
-        var pixScale = 1 / bufferSize * 2;
-        currentProgram.scale(pixScale, -pixScale);
+        gl.viewport(0, 0, bufferWidth, bufferHeight);
+        var pixXScale = 1 / bufferWidth * 2;
+        var pixYScale = 1 / bufferHeight * 2;
+        currentProgram.scale(pixXScale, -pixYScale);
         gl.clearColor(0.0, 0.0, 0.0, 1);
         gl.clear(gl.COLOR_BUFFER_BIT);
         currentProgram.translate(-1, 1);
@@ -91,29 +91,11 @@ function CA(canvas, board) {
     };
 
     function initTexture() {
-        var texPixelArray = new Uint8Array(size * size * 3);
-        var black = new Color(0, 0, 0);
-        var white = new Color(255, 255, 255);
-        board.iteratePositions((x, y, v) => {
-            var idx = (x + y * size) * 3;
-            var color = v === 0 ? black : white;            
-            texPixelArray[idx + 0] = color.r;
-            texPixelArray[idx + 1] = color.g;
-            texPixelArray[idx + 2] = color.b;            
-        });   
+        var srcBufferPixelArray = new Uint8Array(bufferWidth * bufferHeight * 3);
 
-        var srcBufferPixelArray = new Uint8Array(bufferSize * bufferSize * 3);
-        board.iteratePositions((x, y, v) => {
-            var idx = ((y + 1) * bufferSize + 1 + x) * 3;
-            var color = v === 0 ? black : white;            
-            srcBufferPixelArray[idx + 0] = color.r;
-            srcBufferPixelArray[idx + 1] = color.g;
-            srcBufferPixelArray[idx + 2] = color.b;
-        });
-        
         srcTex = gl.createTexture();
         gl.bindTexture(gl.TEXTURE_2D, srcTex);
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, bufferSize, bufferSize, 0, gl.RGB, gl.UNSIGNED_BYTE, srcBufferPixelArray);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, bufferWidth, bufferHeight, 0, gl.RGB, gl.UNSIGNED_BYTE, srcBufferPixelArray);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
@@ -121,7 +103,7 @@ function CA(canvas, board) {
         
         destTex = gl.createTexture();
         gl.bindTexture(gl.TEXTURE_2D, destTex);
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, bufferSize, bufferSize, 0, gl.RGB, gl.UNSIGNED_BYTE, null);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, bufferWidth, bufferHeight, 0, gl.RGB, gl.UNSIGNED_BYTE, null);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
@@ -135,7 +117,7 @@ function CA(canvas, board) {
         gl.bindFramebuffer(gl.FRAMEBUFFER, frameBufferSrc);
         gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, srcTex, 0);
         gl.bindTexture(gl.TEXTURE_2D, srcTex);
-        gl.texSubImage2D(gl.TEXTURE_2D, 0, 0, 0, bufferSize, bufferSize, gl.RGB, gl.UNSIGNED_BYTE, srcBufferPixelArray);
+        gl.texSubImage2D(gl.TEXTURE_2D, 0, 0, 0, bufferWidth, bufferHeight, gl.RGB, gl.UNSIGNED_BYTE, srcBufferPixelArray);
     }
     
     function DrawProgram() {
@@ -221,29 +203,30 @@ function CA(canvas, board) {
                 precision mediump float;
         
                 uniform sampler2D tex;
-                uniform float bs;
+                uniform vec2 bs;
+                uniform vec2 rs;
                 varying vec2 tp;                 
         
-                float cs = 1.0 / bs;
-                float bsEdge = bs - 0.5;
+                vec2 cs = vec2(1.0, 1.0) / bs;
+                vec2 bsEdge = bs - 0.5;
                 
                 void main(void) {
-                   float modx = mod(gl_FragCoord.x, bs);
-                   float mody = mod(gl_FragCoord.y, bs);
-                   if (modx == 0.5 || modx == bsEdge)
-                       discard;
-                   if (mody == 0.5 || mody == bsEdge)
-                       discard;
+                   float modx = mod(gl_FragCoord.x, rs.x);
+                   float mody = mod(bs.y - gl_FragCoord.y, rs.y);
+                   if (modx == 0.5 || gl_FragCoord.x == bsEdge.x || mody == 0.5 || bs.y - gl_FragCoord.y == bsEdge.y) {
+                       gl_FragColor = vec4(0, 0.15, 0.35, 1.0);
+                       return;
+                    }
         
                    float adj = 0.0;
-                   adj += texture2D(tex, vec2(tp.x - cs, tp.y - cs)).r;
-                   adj += texture2D(tex, vec2(tp.x,      tp.y - cs)).r;
-                   adj += texture2D(tex, vec2(tp.x + cs, tp.y - cs)).r;
-                   adj += texture2D(tex, vec2(tp.x - cs, tp.y     )).r;
-                   adj += texture2D(tex, vec2(tp.x + cs, tp.y     )).r;
-                   adj += texture2D(tex, vec2(tp.x - cs, tp.y + cs)).r;
-                   adj += texture2D(tex, vec2(tp.x,      tp.y + cs)).r;
-                   adj += texture2D(tex, vec2(tp.x + cs, tp.y + cs)).r;
+                   adj += texture2D(tex, vec2(tp.x - cs.x, tp.y - cs.y)).r;
+                   adj += texture2D(tex, vec2(tp.x,        tp.y - cs.y)).r;
+                   adj += texture2D(tex, vec2(tp.x + cs.x, tp.y - cs.y)).r;
+                   adj += texture2D(tex, vec2(tp.x - cs.x, tp.y       )).r;
+                   adj += texture2D(tex, vec2(tp.x + cs.x, tp.y       )).r;
+                   adj += texture2D(tex, vec2(tp.x - cs.x, tp.y + cs.y)).r;
+                   adj += texture2D(tex, vec2(tp.x,        tp.y + cs.y)).r;
+                   adj += texture2D(tex, vec2(tp.x + cs.x, tp.y + cs.y)).r;
                    
                    if (texture2D(tex, vec2(tp.x, tp.y)).r == 1.0)
                        gl_FragColor = (adj == 2.0 || adj == 3.0) ? vec4(1.0, 1.0, 1.0, 1.0) : vec4(0.0, 0.0, 0.0, 1.0);
@@ -268,6 +251,7 @@ function CA(canvas, board) {
         var translation = gl.getUniformLocation(drawProgram, "translation");
         var scale = gl.getUniformLocation(drawProgram, "scale");
         var buffSize = gl.getUniformLocation(drawProgram, "bs");
+        var roomSize = gl.getUniformLocation(drawProgram, "rs");
 
         this.posAttr = gl.getAttribLocation(drawProgram, "pos");
         this.texCordAttr = gl.getAttribLocation(drawProgram, "aTexPos");
@@ -288,8 +272,12 @@ function CA(canvas, board) {
             ]));
         };
         
-        this.setCASize = function (size) {
-            gl.uniform1f(buffSize, size);
+        this.setCASize = function (width, height) {
+            gl.uniform2f(buffSize, width, height);
+        };
+        
+        this.setCARoomSize = function (width, height) {
+            gl.uniform2f(roomSize, width, height);
         };
         
         this.use = function() {
@@ -304,7 +292,7 @@ function CA(canvas, board) {
         currentProgram = drawProgram;
         currentProgram.use();
 
-        vertices = new Float32Array([0, bufferSize, bufferSize, bufferSize, 0, 0, bufferSize, bufferSize, 0, 0, bufferSize, 0]);
+        vertices = new Float32Array([0, bufferHeight, bufferWidth, bufferHeight, 0, 0, bufferWidth, bufferHeight, 0, 0, bufferWidth, 0]);
         texCoords = new Float32Array([0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1]);
 
         vertexBuffer = gl.createBuffer();
@@ -395,4 +383,21 @@ function CA(canvas, board) {
             touches = touches.filter(tc => tc.event.identifier !== te.identifier)
         );
     }
+    
+    this.putBoard = function (board, xx, yy) {
+        var bw = board.getWidth();
+        var bh = board.getHeight();
+        var black = new Color(0, 0, 0);
+        var white = new Color(255, 255, 255);
+        var srcBufferPixelArray = new Uint8Array(bw * bh * 3);
+        board.iteratePositions((x, y, v) => {
+            var idx = ((bh - y) * bw + x) * 3;
+            var color = v === 0 ? black : white;            
+            srcBufferPixelArray[idx + 0] = color.r;
+            srcBufferPixelArray[idx + 1] = color.g;
+            srcBufferPixelArray[idx + 2] = color.b;
+        });
+        gl.bindTexture(gl.TEXTURE_2D, srcTex);
+        gl.texSubImage2D(gl.TEXTURE_2D, 0, xx, bufferHeight - yy - bh, bw, bh, gl.RGB, gl.UNSIGNED_BYTE, srcBufferPixelArray);
+    };
 }
